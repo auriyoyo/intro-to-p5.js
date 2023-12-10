f0365b39ece1f2be05ad189a079b064d36a4542c
Lauren Yraola <124393385+auriyoyo@users.noreply.github.com>
2023-12-10 14:12:00 -0600

    draw function

---
+++ script.js
@@ -0,0 +1,9 @@
+function setup() {
+  createCanvas(200, 200);
+  background('pink');
+}
+
+function draw() {
+  fill(245, 208, 214);          // color the circle
+  ellipse(100, 100, 65, 65);    // x, y, width, height
+}
